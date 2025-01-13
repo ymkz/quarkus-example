a6fa73179934b9dcbde86ee8cfc3442240c36386
ymkz <ymkz@users.noreply.github.com>
2025-01-13 18:52:09 +0900

    chore: change openapi servers

--- src/main/java/dev/ymkz/App.java
+++ src/main/java/dev/ymkz/App.java
@@ -5,10 +5,12 @@ import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.servers.Server;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 @OpenAPIDefinition(
     tags = {@Tag(name = "book")},
+    servers = {@Server(url = "http://localhost:8080", description = "ローカル環境")},
     info = @Info(title = "API仕様書", version = "1.0.0"))
 @ApplicationScoped
 @ApplicationPath("api/v1")
