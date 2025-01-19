2bab1b75e2cfe2e10d48eadb3d1839a02d540c67
ymkz <ymkz@users.noreply.github.com>
2025-01-20 00:03:10 +0900

    fix: spotless apply

--- src/main/java/dev/ymkz/presenter/BookResource.java
+++ src/main/java/dev/ymkz/presenter/BookResource.java
@@ -46,8 +46,7 @@ public class BookResource {
       @QueryParam("publishedTimeEnd")
           @Parameter(description = "出版日:終了", example = "2025-01-01T00:00:00Z")
           Instant publishedTimeEnd,
-      @QueryParam("order") @Parameter(description = "並び順") @DefaultValue("-price")
-          BookOrder order,
+      @QueryParam("order") @Parameter(description = "並び順") @DefaultValue("-price") BookOrder order,
       @QueryParam("offset") @Parameter(description = "取得開始位置") @Min(0) @DefaultValue("0")
           Integer offset,
       @QueryParam("limit") @Parameter(description = "取得数") @Min(1) @Max(100) @DefaultValue("100")
